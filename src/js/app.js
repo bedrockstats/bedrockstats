91d4d473fdc55ba0129e81e1ff3058f832529634
Plextora <plextoraa.a@gmail.com>
2022-07-20 16:09:58 -0400

    Get stats button will not console log the user's stats

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,8 @@ const userInput = document.getElementById("userInput");
 
 function getStats() {
   const username = userInput.value; // this will be used in the api request
+
+  fetch(`https://api.hyperlandsmc.net/stats/${username}`)
+    .then((response) => response.json())
+    .then((json) => console.log(json.stats));
 }
