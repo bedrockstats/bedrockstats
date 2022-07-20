e9cc6035201708d4c4ef7aa833c841d8dc7c986d
Plextora <plextoraa.a@gmail.com>
2022-07-20 17:00:51 -0400

    Added functionality for bedwars stats

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,30 @@
 const userInput = document.getElementById("userInput");
 
+// get bw stats elements
+const bwBedsBroken = document.getElementById("bwBedsBroken");
+const bwBestWinstreak = document.getElementById("bwBestWinstreak");
+const bwCurrentWinstreak = document.getElementById("bwCurrentWinstreak");
+const bwFinalKills = document.getElementById("bwFinalKills");
+const bwKills = document.getElementById("bwKills");
+const bwWins = document.getElementById("bwWins");
+
 function getStats() {
   const username = userInput.value; // this will be used in the api request
 
   fetch(`https://api.hyperlandsmc.net/stats/${username}`)
     .then((response) => response.json())
-    .then((json) => console.log(json.stats));
+    .then((json) => fillStats(json.stats));
+}
+
+function fillStats(json) {
+  console.log(json);
+  console.log(json.bedwars);
+
+  // bw stats
+  bwBedsBroken.textContent = `Beds broken: ${json.bedwars.bedsBroken}`;
+  bwBestWinstreak.textContent = `Best winstreak: ${json.bedwars.bestWinstreak}`;
+  bwCurrentWinstreak.textContent = `Current winstreak: ${json.bedwars.currentWinstreak}`;
+  bwFinalKills.textContent = `Final Kills: ${json.bedwars.finalKills}`;
+  bwKills.textContent = `Kills: ${json.bedwars.kills}`;
+  bwWins.textContent = `Wins: ${json.bedwars.wins}`;
 }
