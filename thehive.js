fd2868466ce1e8c977adcc6121a923913d26e07a
Plextora <plextoraa.a@gmail.com>
2022-07-22 12:08:01 -0400

    Made stats collapsible on The Hive stats page

--- thehive.js
+++ thehive.js
@@ -21,6 +21,22 @@ function handleErrors(response) {
   return response;
 }
 
+let coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    content.style.display = "grid";
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.display = "inline";
+    }
+  });
+}
+
 function getStats() {
   const username = userInput.value; // this will be used in the api request
 
